b7682c836b3d7e5753ff876b6c7c1884d90b963f
tillepille <github@tillepille.io>
2021-11-27 17:36:08 +0100

    fmt

--- installation/crossplane.tf
+++ installation/crossplane.tf
@@ -1,17 +1,17 @@
 provider "helm" {
   kubernetes {
     host                   = digitalocean_kubernetes_cluster.crossplane.kube_config[0].host
-    token             = digitalocean_kubernetes_cluster.crossplane.kube_config[0].token
+    token                  = digitalocean_kubernetes_cluster.crossplane.kube_config[0].token
     cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.crossplane.kube_config[0].cluster_ca_certificate)
   }
 }
 
 resource "helm_release" "crossplane" {
-  name       = "do-crossplane"
-  repository = "https://charts.crossplane.io/stable"
-  chart      = "crossplane"
-  version    = "1.5.0"
-  namespace = "crossplane-system"
-  create_namespace =  true
+  name              = "do-crossplane"
+  repository        = "https://charts.crossplane.io/stable"
+  chart             = "crossplane"
+  version           = "1.5.0"
+  namespace         = "crossplane-system"
+  create_namespace  = true
   dependency_update = true
 }
